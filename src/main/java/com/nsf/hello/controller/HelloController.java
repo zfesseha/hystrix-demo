f5da251a6a537869eaa035ec51c23afb79bbd4b1
ZeleAlem Fesseha <zfesseha1@gmail.com>
2017-10-13 15:41:57 -0400

    Updated triple-once endpoint to throttle every 5 seconds.

--- src/main/java/com/nsf/hello/controller/HelloController.java
+++ src/main/java/com/nsf/hello/controller/HelloController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
@@ -14,10 +16,15 @@ import java.util.Set;
 @RequestMapping("/api/v1")
 public class HelloController {
 
-    private String[] names = new String[] {"Adam", "Bob", "Chelsea", "Denise", "Eve", "Frank"};
-    private String[] greetings = new String[] {"Hello", "Hi", "Hey", "What's up"};
+    private String[] names = new String[] {"Adam", "Bob", "Chelsea", "Diana", "Eve", "Frank", "Gina"};
+    private String[] greetings = new String[] {"Hello", "Hi", "Hey", "What's up", "Greetings", "Good day"};
     private static final Random random = new Random();
     private final Set<Integer> tripleRequests = new HashSet();
+    private final Map<Integer, Long> pastRequests = new HashMap();
+    
+    private static final Long MILLIS_IN_SEC = 1000l;
+    private static final Long REQUEST_THRESHOLD_SEC = 5l;
+    private static final Long REQUEST_THRESHOLD_MILLIS = REQUEST_THRESHOLD_SEC * MILLIS_IN_SEC;
 
     @RequestMapping(method = RequestMethod.GET, value = "/greeting")
     public Greeting greeting() {
@@ -31,12 +38,18 @@ public class HelloController {
         return 3 * number;
     }
 
+    // TODO: Update the route, method name and corresponding client methods to throttled-triple
     @RequestMapping(method = RequestMethod.GET, value = "/triple-once/{number}")
     public Integer tripleOnce(@PathVariable("number") Integer number) {
-        if (tripleRequests.contains(number)) {
-            throw new IllegalArgumentException("A request has been made to triple " + number + " already.");
+    		Long lastRequest = pastRequests.get(number);
+    		Long now = System.currentTimeMillis();
+        if (lastRequest != null && (now - lastRequest < REQUEST_THRESHOLD_MILLIS)) {
+            throw new IllegalArgumentException("A request to triple " + number + " was made "
+            		+ ((double) (now - lastRequest)/MILLIS_IN_SEC) + " seconds ago.\nPlease wait " + REQUEST_THRESHOLD_SEC +
+            		" seconds before making a request to triple the same number.");
         }
         tripleRequests.add(number);
+        pastRequests.put(number, now);
         return 3 * number;
     }
 }
