8b225439454b02ba19a2823ef348a35fe9c2c6c6
ZeleAlem Fesseha <zfesseha1@gmail.com>
2017-10-13 11:33:51 -0400

    Adds a triple once endpoint that returns a triple of a number only once.

--- src/main/java/com/nsf/hello/controller/HelloController.java
+++ src/main/java/com/nsf/hello/controller/HelloController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -15,6 +17,7 @@ public class HelloController {
     private String[] names = new String[] {"Adam", "Bob", "Chelsea", "Denise", "Eve", "Frank"};
     private String[] greetings = new String[] {"Hello", "Hi", "Hey", "What's up"};
     private static final Random random = new Random();
+    private final Set<Integer> tripleRequests = new HashSet();
 
     @RequestMapping(method = RequestMethod.GET, value = "/greeting")
     public Greeting greeting() {
@@ -25,6 +28,15 @@ public class HelloController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/triple/{number}")
     public Integer triple(@PathVariable("number") Integer number) {
+        return 3 * number;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/triple-once/{number}")
+    public Integer tripleOnce(@PathVariable("number") Integer number) {
+        if (tripleRequests.contains(number)) {
+            throw new IllegalArgumentException("A request has been made to triple " + number + " already.");
+        }
+        tripleRequests.add(number);
         return 3 * number;
     }
 }
